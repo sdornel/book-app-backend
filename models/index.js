9a84034b64cacac0e01fab8454015c80b4d49d08
kaidornel <kai_dornel@hotmail.com>
2020-08-27 19:32:31 -0400

    added user model

--- models/index.js
+++ models/index.js
@@ -19,6 +19,7 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.tutorials = require("./user.js")(sequelize, Sequelize);
+db.users = require("./user.js")(sequelize, Sequelize);
+// db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 
 module.exports = db;
