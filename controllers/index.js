8e75d51814c506678dd94104cb53982d68df82ae
kaidornel <kai_dornel@hotmail.com>
2020-09-01 18:47:44 -0400

    created book controllers

--- controllers/index.js
+++ controllers/index.js
@@ -137,15 +137,82 @@ const deleteReview = async (req, res) => {
     }
 }
 
-const createBook
+const createBook = async (req, res) => {
+    try {
+        const book = await Book.create(req.body);
+        return res.status(201).json({
+            book,
+        });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
 
-const getAllBooks
+const getAllBooks = async (req, res) => {
+    try {
+        const books = await Book.findAll({
+            include: [
+                {
+                    model: Review
+                }
+            ]
+        });
+        return res.status(200).json({ books });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
 
-const getBookById
+const getBookById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const book = await Book.findOne({
+            where: { id: id },
+            include: [
+                {
+                    model: Review
+                }
+            ]
+        });
+        if (book) {
+            return res.status(200).json({ book });
+        }
+        return res.status(404).send('Book with the specified ID does not exist');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
 
-const updateBook
+const updateBook = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await Book.update(req.body, { // check if [] is needed
+            where: { id: id }
+        });
+        if (updated) {
+            const updatedBook = await Book.findOne({ where: { id: id } });
+            return res.status(200).json({ review: updatedBook });
+        }
+        throw new Error('Book not found');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
 
-const deleteBook
+const deleteBook = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await Book.destroy({
+            where: { id: id }
+        });
+        if (deleted) {
+            return res.status(204).send("Book deleted");
+        }
+        throw new Error("Book not found");
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
 
 module.exports = {
     createUser,
