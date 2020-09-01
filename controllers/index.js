c5f497c69570a8e8547ab202082dedf9556267c7
kaidornel <kai_dornel@hotmail.com>
2020-08-31 22:57:57 -0400

    review controller done

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const { User, Review } = require('../models');
+const { User, Review, Book } = require('../models');
 const createUser = async (req, res) => {
     try {
         const user = await User.create(req.body);
@@ -23,7 +23,130 @@ const getAllUsers = async (req, res) => {
         return res.status(500).send(error.message);
     }
 }
+const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findOne({
+            where: { id: id },
+            include: [
+                {
+                    model: Review
+                }
+            ]
+        });
+        if (user) {
+            return res.status(200).json({ user });
+        }
+        return res.status(404).send('User with the specified ID does not exist');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await User.update(req.body, {
+            where: { id: id }
+        });
+        if (updated) {
+            const updatedUser = await User.findOne({ where: { id: id } });
+            return res.status(200).json({ user: updatedUser });
+        }
+        throw new Error('User not found');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await User.destroy({
+            where: { id: id }
+        });
+        if (deleted) {
+            return res.status(204).send("User deleted");
+        }
+        throw new Error("User not found");
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+
+
+const createReview = async (req, res) => {
+    try {
+        const review = await Review.create(req.body);
+        return res.status(201).json({
+            review,
+        });
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+const getAllReviews = async (req, res) => {
+    try {
+        const reviews = await Review.findAll()
+        return res.status(200).json({ reviews });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+const getReviewById = async (req, res) => {
+    try {
+        const review = await Review.findOne({
+            where: { id: id},
+        })
+        if (review) {
+            return res.status(200).json({ review });
+        }
+        return res.status(404).send('Review with the specified ID does not exist');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const updateReview = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await Review.update(req.body, {
+            where: { id: id }
+        });
+        if (updated) {
+            const updatedReview = await Review.findOne({ where: { id: id } });
+            return res.status(200).json({ review: updatedReview });
+        }
+        throw new Error('Review not found');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const deleteReview = async (req, res) => { 
+    try {
+        const { id } = req.params;
+        const deleted = await Review.destroy({
+            where: { id: id }
+        });
+        if (deleted) {
+            return res.status(204).send("Review deleted");
+        }
+        throw new Error("Review not found");
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
 module.exports = {
     createUser,
-    getAllUsers
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+
+    createReview,
+    getAllReviews,
+    getReviewById,
+    updateReview,
+    deleteReview
 }
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,15 @@ router.get('/', (req, res) => res.send('This is root!'))
 
 
 router.get('/users', controllers.getAllUsers)
+router.get('/users/:id', controllers.getUserById)
+// router.put('/users/:id', controllers.updateUser) // you always use patch. should try to figure out why tutorial uses put...
+router.patch('/users/:id', controllers.updateUser)
+router.delete('/users/:id', controllers.deleteUser)
 router.post('/users', controllers.createUser)
+
+router.get('/reviews', controllers.getAllReviews)
+router.post('/users/', controllers.updateUser)
+
+
 
 module.exports = router
