e2f9de4bd94b2d03ae4682910ef341f52f6cfafb
kaidornel <kai_dornel@hotmail.com>
2020-09-09 18:45:22 -0400

    passportjs updated

--- services/passport.js
+++ services/passport.js
@@ -6,3 +6,21 @@ const ExtractJwt = require('passport-jwt').ExtractJwt
 const {getUserById} = require('../controllers/index')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcrypt')
+
+// create local strategy
+
+const localOptions = {usernameField: 'email'}
+
+const localOptions = new LocalStrategy(localOptions, (email, password, done) => {
+    return verifyUser(email)
+    .then((validUser) => {
+        bcrypt.compare(password, validUser.password)
+        .then((validPassword) => {
+            if(validPassword) {
+                return done(null, validUser)
+            }
+            return done(null, false)
+        })
+        .catch(err => done(err, false))
+    })
+})
