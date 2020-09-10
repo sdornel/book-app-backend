341e93bfd91239cc0474a7a551ac6ee8f9481cce
kaidornel <kai_dornel@hotmail.com>
2020-09-09 20:51:47 -0400

    modified user seed passwords

--- seeders/20200829180258-users.js
+++ seeders/20200829180258-users.js
@@ -3,21 +3,21 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [{
       name: 'John',
       email: 'john@doe.com',
-      password: '123456789',
+      password: 'one',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       name: 'Smith',
       email: 'john@smith.com',
-      password: '123456789',
+      password: 'two',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       name: 'Stone',
       email: 'john@stone.com',
-      password: '123456789',
+      password: 'three',
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
