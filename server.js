37a7a4f898a19c753a7902c8849215caf4c712f4
kaidornel <kai_dornel@hotmail.com>
2020-09-18 13:39:24 -0400

    removed old export

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser')
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy;
 const cors = require('cors')
 
 const PORT = process.env.PORT || 3000;
